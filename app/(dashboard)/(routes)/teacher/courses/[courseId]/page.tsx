3e8490d77f37700599585843e93360678cfd3038
mastergamer17000 <mastergamer17000@gmail.com>
2024-05-29 21:57:03 +0000

    cuarto commit

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { Titleform } from "./_components/title-form";
 
 const CourseIdPage = async ({
     params
@@ -59,11 +60,14 @@ const CourseIdPage = async ({
          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                  <div>
                   <div className="flex items-center gap-x-2">
-                    <IconBadge icon={LayoutDashboard}/>
+                    <IconBadge size="sm" icon={LayoutDashboard}/>
                     <h2 className="text-xl">
                       Customize your course
                     </h2>
                   </div>
+                  <Titleform
+                  initialData={course}
+                  courseId={course.id}/>
                  </div>
          </div>
         </div>
